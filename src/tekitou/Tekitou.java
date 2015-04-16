1b8271516af6ccb56105dd17acddd7a2de4ef642
x14g001 <x14g001@PC505N001.chiba-fjb.ac.jp>
2015-04-16 15:06:55 +0900

    あ

---
+++ src/tekitou/Tekitou.java
@@ -0,0 +1,12 @@
+package tekitou;
+
+public class Tekitou {
+
+	public static void main(String[] args) {
+
+		int i;
+		//iを0に初期設定;i<3を満たしていたら処理を実行;処理終了後、iに1を足す
+		for(i=0;i<3;i=i+1)  
+		    System.out.println(i);  //iの値を出力し改行
+	}
+}
